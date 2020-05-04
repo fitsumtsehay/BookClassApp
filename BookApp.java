af7e70830c7ea0f2d731558c0fea8cf603f7645f
Fitsum <63597726+fitsumtsehay@users.noreply.github.com>
2020-05-04 12:02:39 -0400

    Update BookApp.java

--- BookApp.java
+++ BookApp.java
@@ -2,9 +2,10 @@ public class BookApp {
 
    public static void main(String[] args){
         Book3 book3=new Book3();
-       book3.setTitle("Java Book");
+        book3.setAuthor("John");
+        book3.setTitle("Java Book");
+        book3.setDescription("Object oriented programming");
         System.out.println(book3.getTitle());
         System.out.println(book3.getTitle());
-
    }
 }
